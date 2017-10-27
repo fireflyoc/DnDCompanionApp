f0454987272471f3dfb40b643d05b68a691af90e
Noah <fireflyoc@gmail.com>
2017-10-27 11:24:56 -0500

    Added in Navigation Drawer (Swipe right to open) in main menu.

---
+++ app/src/main/java/edu/uco/ngeorge/dndcompanionapp/CharSheetActivity.java
@@ -0,0 +1,16 @@
+package edu.uco.ngeorge.dndcompanionapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Noah G on 10/27/2017.
+ */
+
+public class CharSheetActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.char_sheet_stats);
+    }
+}
